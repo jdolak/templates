3446f316e9a2ac8d1707b2f6fc0d541e5d60f3d3
Jachob Dolak <51254246+jdolak@users.noreply.github.com>
2025-03-26 22:51:32 -0400

    Update bashrc.sh

--- bashrc.sh
+++ bashrc.sh
@@ -1,10 +1,10 @@
 cat << EOF >> .bashrc
-parse_git_branch() {
-  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+show_git_branch() {
+  git rev-parse --abbrev-ref HEAD 2> /dev/null
 }
 
 # Custom Shell
-PS1="\n\[\033[01;32m\]\h\[\033[00m\]: \[\033[01;34m\]\w\[\033[00m\]\$(parse_git_branch)\n\[\033[01;32m\]\$\[\033[00m\] "
+PS1="\n\[\033[01;32m\]\h\[\033[00m\]: \[\033[01;34m\]\w\[\033[00m\] (\$(show_git_branch)) \n\[\033[01;32m\]$\[\033[00m\] "
 
 EDITOR=/usr/bin/vim
 
